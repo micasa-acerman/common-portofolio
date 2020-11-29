2697c06c0c93dd7c565b11020ce491dd2875db3a
Micasa Acerman <koha009@yandex.ru>
2020-11-29 16:57:07 +0300

    templates: index page design

--- src/templates/index.js
+++ src/templates/index.js
@@ -1,12 +1,12 @@
 /* eslint react/prop-types: 0 */
-import React from 'react';
-import Link from 'gatsby-link';
+import React from "react";
+import Link from "gatsby-link";
 
-import Card from '../components/Card';
-import Sidebar from '../components/Sidebar';
-import ShareBox from '../components/ShareBox';
+import Card from "../components/Card";
+import Sidebar from "../components/Sidebar";
+import ShareBox from "../components/ShareBox";
 
-import './index.scss';
+import "./index.scss";
 
 const NavLinkText = ({ color, text }) => (
   <div
@@ -32,13 +32,10 @@ const NavLink = ({ test, url, text }) => {
 };
 
 const Page = ({ pageContext, location }) => {
-  const {
-    group, index, first, last, pathPrefix,
-  } = pageContext;
+  const { group, index, first, last, pathPrefix } = pageContext;
 
-  const previousUrl = index - 1 === 1 ? '' : `/${pathPrefix}/${index - 1}`;
+  const previousUrl = index - 1 === 1 ? "" : `/${pathPrefix}/${index - 1}`;
   const nextUrl = `/${pathPrefix}/${index + 1}`;
-
   return (
     <React.Fragment>
       <div
@@ -49,28 +46,42 @@ const Page = ({ pageContext, location }) => {
       >
         <Sidebar />
         <div className="col-xl-6 col-lg-7 col-md-12 col-xs-12 order-2">
-          {group.map(({ node }) => (
-            <Card {...node.frontmatter} url={node.frontmatter.slug ? node.frontmatter.slug : node.fields.slug} key={node.fields.slug} />
-          ))}
+          <div className="row">
+            {group.map(({ node }) => (
+              <Card
+                {...node.frontmatter}
+                url={
+                  node.frontmatter.slug
+                    ? node.frontmatter.slug
+                    : node.fields.slug
+                }
+                key={node.fields.slug}
+              />
+            ))}
+          </div>
 
           <div
             className="row"
             style={{
-              justifyContent: 'space-around',
-              marginBottom: '20px',
+              justifyContent: "space-around",
+              marginBottom: "20px",
             }}
           >
-            <div className="previousLink">
-              <NavLink test={!first} url={previousUrl} text="Previous" />
-            </div>
-            <div className="nextLink">
-              <NavLink test={!last} url={nextUrl} text="Next" />
-            </div>
+            {!first && (
+              <div className="previousLink">
+                <NavLink test={!first} url={previousUrl} text="Previous" />
+              </div>
+            )}
+            {!last && (
+              <div className="nextLink">
+                <NavLink test={!last} url={nextUrl} text="Next" />
+              </div>
+            )}
           </div>
         </div>
         <div className="col-xl-2 col-lg-1 order-3" />
       </div>
-      <ShareBox url={location.href} hasCommentBox={false} />
+      {/* <ShareBox url={location.href} hasCommentBox={false} /> */}
     </React.Fragment>
   );
 };
