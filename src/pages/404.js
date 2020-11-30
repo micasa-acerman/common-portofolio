82de3bdbc53b9e208bf629b479cb36766888f835
Micasa <koha009@yandex.ru>
2020-12-01 00:26:00 +0300

    pages: update 404 template

--- src/pages/404.js
+++ src/pages/404.js
@@ -7,7 +7,7 @@ const NotFoundPage = ({ data }) => (
   <div className="container">
     <div className="row">
       <div className="col">
-        <h4>找不到你的網頁，本站所有頁面為：</h4>
+        <h4>Не удалось найти страницу</h4>
         {data.allSitePage.edges.map(page => (
           <Link to={page.node.path} href={page.node.path} key={page.node.path}>
             <li>{page.node.path}</li>
