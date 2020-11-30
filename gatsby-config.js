7b472c7202d51bd21811c6ff33b65992e685ddf4
Micasa <koha009@yandex.ru>
2020-12-01 00:25:09 +0300

    common: favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -64,12 +64,12 @@ module.exports = {
         display: 'standalone',
         icons: [
           {
-            src: '/favicons/android-chrome-192x192.png',
+            src: '/common-portofolio/favicons/android-chrome-192x192.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: '/favicons/android-chrome-512x512.png',
+            src: '/common-portofolio/favicons/android-chrome-512x512.png',
             sizes: '512x512',
             type: 'image/png',
           },
